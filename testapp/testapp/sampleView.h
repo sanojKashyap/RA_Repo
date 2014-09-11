88dfdf888301f323364d39a69520a11443525856
Sandy <sanoj.kashyap@gmail.com>
2014-09-12 00:31:52 +0530

    new chafes, new file added

---
+++ testapp/testapp/sampleView.h
@@ -0,0 +1,13 @@
+//
+//  sampleView.h
+//  testapp
+//
+//  Created by Sanoj Kashyap on 12/09/14.
+//  Copyright (c) 2014 Sanoj Kashyap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sampleView : UIView
+
+@end
---
+++ testapp/testapp/sampleView.m
@@ -0,0 +1,31 @@
+//
+//  sampleView.m
+//  testapp
+//
+//  Created by Sanoj Kashyap on 12/09/14.
+//  Copyright (c) 2014 Sanoj Kashyap. All rights reserved.
+//
+
+#import "sampleView.h"
+
+@implementation sampleView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
