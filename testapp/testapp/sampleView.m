21f0450e22fefd45ed53956bb635707db1be308e
Sandy <sanoj.kashyap@gmail.com>
2014-09-12 00:33:19 +0530

    method added

--- testapp/testapp/sampleView.m
+++ testapp/testapp/sampleView.m
@@ -19,6 +19,11 @@
     return self;
 }
 
+
+-(NSString*)returnName
+{
+    return @"sanoj";
+}
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
